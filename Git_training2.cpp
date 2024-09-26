fc28c65b9b8642b9f70bea8bd880d682d96a75ee
Maksym <hmel408757595@outlook.com>
2024-09-26 10:28:29 +0300

    first commit

---
+++ Git_training2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+    setlocale(0, "");
+
+    const int SIZE = 10;
+    int ar[SIZE];
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        ar[i] = rand() % 100;
+        cout << ar[i] << ", ";
+    }
+    cout << "\n\n";
+}
